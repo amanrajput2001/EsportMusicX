b18049ee9f7705cf7a2217d9f017a8182dabf000
Mr Smoker <97607922+SmokerMusicX@users.noreply.github.com>
2022-02-24 17:18:06 +0530

    Delete database directory

--- database/dblocal.py
+++
@@ -1,8 +0,0 @@
-""" mongo database """
-
-from motor.motor_asyncio import AsyncIOMotorClient as Bot
-from config import MONGODB_URL as aditya
-
-
-MONGODB_CLI = Bot(aditya)
-db = MONGODB_CLI.program
